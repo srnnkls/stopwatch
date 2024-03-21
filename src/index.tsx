64322f69f4eb6392f7e3475ac144773ff09929c6
Sören Nikolaus <31724172+srnnkls@users.noreply.github.com>
2024-03-21 20:42:15 +0000

    Add global debug event listener

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,23 @@ import "src/style/global.css";
 
 import App from "src/components/App";
 
+const handleMessage = (event: MessageEvent) => {
+  if (event.origin === 'aaa') {
+    console.log('Received message:', event.data);
+  }
+  console.log('Not trusted!');
+};
+
+window.addEventListener('message', handleMessage);
+
+
 ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
   document.getElementById("root")
 );
+
+window.addEventListener('beforeunload', () => {
+  window.removeEventListener('message', handleMessage);
+});
